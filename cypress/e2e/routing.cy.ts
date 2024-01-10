164b9d613b2661c7561d148fa337d30dee7f7856
Alexey Gamov <lexa2009xxxx@yandex.ru>
2024-01-11 00:01:20 +0300

    feat: test cypress access & routing

---
+++ cypress/e2e/routing.cy.ts
@@ -0,0 +1,27 @@
+const PAGES = [
+  { name: 'Строка', url: '/recursion' },
+  { name: 'Фибоначчи', url: '/fibonacci' },
+  { name: 'Сортировка', url: '/sorting' },
+  { name: 'Стек', url: '/stack' },
+  { name: 'Очередь', url: '/queue' },
+  { name: 'Связный список', url: '/list' },
+];
+
+describe('app works correctly with routes', function () {
+  beforeEach(function () {
+    cy.visit('http://localhost:3000');
+  });
+
+  it('Должна открыться главная', () => {
+    cy.contains('МБОУ АЛГОСОШ');
+  });
+
+  PAGES.forEach((page) => {
+    it(`Должна открыться страница ${page.name}`, () => {
+      cy.get(`a[href*='${page.url}']`).click();
+      cy.contains(`${page.name}`);
+      cy.get('button').contains('К оглавлению').click();
+      cy.contains('МБОУ АЛГОСОШ');
+    });
+  });
+});
